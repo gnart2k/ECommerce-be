112ab6b4b213ed6d8f1192e7407168e256c0cb06
trang <trangbg2000@gmail.com>
2023-10-20 22:49:23 +0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
@@ -15,6 +16,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 dbConnect();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
